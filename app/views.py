47c63c971fdf4ff1a83ed2b2417af8345a0a8b79
yzd <404013419@qq.com>
2016-08-27 15:57:24 +0800

    base模板完成

--- app/views.py
+++ app/views.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 from app import app
+from flask import render_template
+
 @app.route('/')
 @app.route('/index')
+
 def index():
-    user = {'nickname': 'Miguel'}
-    return '''
-        <html>
-          <head>
-            <title>Home Page</title>
-          </head>
-          <body>
-            <h1>Hello, ''' + user['nickname'] + '''</h1>
-          </body>
-        </html>
-        '''
+    user = {'nickname': 'Yang'}  # 用户名
+    posts = [  # 提交内容
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
