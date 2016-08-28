41be4931d3108f6557d26c28d3be0525d1ee88d0
yzd <404013419@qq.com>
2016-08-28 11:47:52 +0800

    Day3 database

--- app/models.py
+++ app/models.py
@@ -6,11 +6,20 @@ ROLE_USER = 0
 ROLE_ADMIN = 1
 
 class User(db.Model):
-    """每一个属性定义一个字段"""
     id = db.Column(db.Integer, primary_key = True)
-    nickname = db.Column(db.String(64), index = True, unique = True)
-    email = db.Column(db.String(120), index = True, unique = True)
+    nickname = db.Column(db.String(64), unique = True)
+    email = db.Column(db.String(120), unique = True)
     role = db.Column(db.SmallInteger, default = ROLE_USER)
+    posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
