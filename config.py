b4478ec4adb8a766ac27db4f99c657174097fec2
yzd <404013419@qq.com>
2016-08-29 18:08:10 +0800

    Day4 完工

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 SECRET_KEY = 'yangzhendong'
 
+# 数据库相关
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-#!flask/bin/python
+# !flask/bin/python
 from app import app
-app.run(host='0.0.0.0', debug=True, port=9999)
+app.run(debug=True, port=8765)
