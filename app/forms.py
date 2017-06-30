15234392c8c21c1501de6c67349f7a9fd410c491
YangZhendong <404013419@qq.com>
2017-06-30 14:36:46 -0600

    添加商品完成 待完成：删除单个条目

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask_wtf import Form
 from wtforms import StringField, BooleanField, PasswordField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
@@ -6,10 +7,22 @@ from wtforms.validators import DataRequired, Email, Length
 class LoginForm(Form):
     user_name = StringField('user name', validators=[DataRequired(), Length(max=15)])
     remember_me = BooleanField('remember me', default=False)
-    submit = SubmitField('Log in')
+    submit = SubmitField('登录'.decode('utf-8'))
 
 
 class SignUpForm(Form):
     user_name = StringField('user name', validators=[DataRequired(), Length(max=15)])
     user_email = StringField('user email', validators=[Email(), DataRequired(), Length(max=128)])
-    submit = SubmitField('Sign up')
+    submit = SubmitField('注册'.decode('utf-8'))
+
+
+class AboutMeForm(Form):
+    describe = TextAreaField('about me', validators=[DataRequired(), Length(max=140)])
+    submit = SubmitField('YES!')
+
+
+class AddMonitorItemForm(Form):
+    item_id = TextAreaField('item_id', validators=[DataRequired()])
+    user_price = TextAreaField('item_id', validators=[DataRequired()])
+    mall_id = TextAreaField('item_id', validators=[DataRequired()])
+    submit = SubmitField('添加监控商品'.decode('utf-8'))
--- app/views.py
+++ app/views.py
@@ -3,23 +3,20 @@ from app import app, db, lm
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
 from models import User, Monitor, ROLE_USER, ROLE_ADMIN
-from forms import LoginForm, SignUpForm
+from forms import LoginForm, SignUpForm, AboutMeForm, AddMonitorItemForm
 import datetime
+from string import strip
+
+
+@app.route('/monitor')
+def to_monitor():
+    return render_template('monitor.html', title='Monitor')
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template(
-        "index.html",
-        title="Home",
-        )
-
-
-@app.route('/all')
-def show_all():
-    user = User.query.order_by(User.user_id)
-    return render_template('all.html', user=user)
+    return render_template("index.html", title="Home")
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -39,22 +36,18 @@ def login():
                 db.session.add(user)
                 db.session.commit()
             except:
-                flash("The Database error!")
+                flash("数据库读取错误，请重试")
                 return redirect('/login')
 
-            flash(request.form.get('user_name'))
+            # flash(request.form.get('user_name'))
             # flash('remember me? ' + str(request.form.get('remember_me')))
             return redirect(url_for("index", user_id=current_user.id))
         else:
-            flash('Login failed, Your name is not exist!')
+            flash('没有该用户，请注册')
             return redirect('/login')
 
     return render_template("login.html", title="Sign In", form=form)
 
-
-@app.route('/monitor')
-def to_monitor():
-    return render_template('monitor.html', title='Monitor')
 
 
 @lm.user_loader
@@ -80,7 +73,7 @@ def sign_up():
         register_check = User.query.filter(db.or_(
             User.nickname == user_name, User.email == user_email)).first()
         if register_check:
-            flash("error: The user's name or email already exists!")
+            flash("用户名或邮箱重复")
             return redirect('/sign-up')
 
         if len(user_name) and len(user_email):
@@ -91,12 +84,61 @@ def sign_up():
                 db.session.add(user)
                 db.session.commit()
             except:
-                flash("The Database error!")
+                flash("数据库报错请重试")
                 return redirect('/sign-up')
 
-            flash("Sign up successful!")
-            return redirect('/index')
+            flash("登陆成功")
+            return redirect('/login')
 
-    return render_template(
-        "sign_up.html",
-        form=form)
+    return render_template("sign_up.html", form=form)
+
+
+@app.route('/user/<int:user_id>', methods=["POST", "GET"])
+@login_required
+def users(user_id):
+    form = AboutMeForm()
+    user = User.query.filter(User.id == user_id).first()
+    if not user:
+        flash("The user is not exist.")
+        redirect("/index")
+    all_item = user.all_item.all()
+
+    return render_template("user.html", form=form, user=user, all_item=all_item)
+
+
+@app.route('/addmonitoritem/<int:user_id>', methods=["POST", "GET"])
+@login_required
+def addmonitoritem(user_id):
+    form = AddMonitorItemForm()
+    item = Monitor()
+    if form.validate_on_submit():
+        item_id = request.form.get("item_id")
+        if not len(strip(item_id)):
+            flash("The content is necessray!")
+            return redirect(url_for("addmonitoritem", user_id=user_id))
+        user_price = request.form.get("user_price")
+        if not len(strip(user_price)):
+            flash("The content is necessray!")
+            return redirect(url_for("addmonitoritem", user_id=user_id))
+        mall_id = request.form.get("mall_id")
+        if not len(strip(item_id)):
+            flash("The content is necessray!")
+            return redirect(url_for("addmonitoritem", user_id=user_id))
+        item.item_id = item_id
+        item.user_price = user_price
+        item.mall_id = mall_id
+        item.add_date = datetime.datetime.now()
+        item.user_id = user_id
+        item.status = True
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except:
+            flash("数据库报错请重试")
+            return redirect(url_for("addmonitoritem", user_id=user_id))
+
+        flash("添加商品成功")
+        return redirect(url_for("users", user_id=user_id))
+
+    return render_template("addmonitoritem.html", form=form)
